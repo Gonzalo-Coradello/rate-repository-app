e186c130de8f8ed306108b2e614e823e9b3badaa
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-11-02 10:23:34 -0300

    refactor query to use a fragment

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,29 +1,16 @@
+import { REPOSITORY_DETAILS } from './fragments'
+
 const { gql } = require('@apollo/client')
-
-// export const GET_REPOSITORIES = gql`
-//   query {
-//     repositories {
-
-//     }
-//   }
-// `
 
 export const GET_REPOSITORIES = gql`
   query {
     repositories {
       edges {
         node {
-          id
-          fullName
-          description
-          language
-          forksCount
-          stargazersCount
-          ratingAverage
-          reviewCount
-          ownerAvatarUrl
+          ...RepositoryDetails
         }
       }
     }
   }
+  ${REPOSITORY_DETAILS}
 `
